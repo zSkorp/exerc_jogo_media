685c3ba7a8f0bb7004fe46ce0290e9ee51787424
Fernando Milano <131847205+zSkorp@users.noreply.github.com>
2023-06-05 14:41:29 -0300

    Add files via upload

---
+++ acertou_perdeu.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace campo_minado
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int limiteinicial;
+            int limitefinal;
+            int numoculto;
+            int jogada;
+            int qtdplayers;
+            int player = 0;
+
+            Console.WriteLine("Jogo: Acertou, perdeu!");
+            Console.Write("\nDigite a quantidade de jogadores: ");
+            qtdplayers = int.Parse(Console.ReadLine());
+            string[] players = new string[qtdplayers];
+
+            for (int i = 0; i < qtdplayers; i++)
+            {
+                Console.Write("\nDigite o nome do {0}º jogador: ", i + 1);
+                players[i] = Console.ReadLine();
+            }
+
+            Random random = new Random();
+            numoculto = random.Next(1, 101);
+
+            Console.Clear();
+            Console.WriteLine("Jogo: Acertou, perdeu!");
+            limiteinicial = 1;
+            limitefinal = 100;
+
+            do
+            {
+                do
+                {
+                    do
+                    {
+                        Console.Write("\n{0}, digite um número entre {1} e {2}: ", players[player], limiteinicial, limitefinal);
+                        jogada = int.Parse(Console.ReadLine());
+                    } while (jogada < limiteinicial || jogada > limitefinal);
+                    player = (player + 1) % qtdplayers;
+                } while (jogada <= limiteinicial || jogada >= limitefinal);
+
+                if (jogada == numoculto)
+                {
+                    Console.WriteLine("\n{0} acertou o número e perdeu, parabéns!\n", players[player - 1]);
+                }
+                else
+                {
+                    if (jogada < numoculto)
+                        limiteinicial = jogada;
+                    else
+                        limitefinal = jogada;
+                }
+            } while (jogada != numoculto);
+        }
+    }
+}
---
+++ media_valores.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace media_valores
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] valores = new int[10];
+            int soma = 0;
+            int contador = 0;
+            double media;
+
+            Console.WriteLine("Média aritmética de 10 valores inteiros\n");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0}º valor: ", i + 1);
+                valores[i] = int.Parse(Console.ReadLine());
+                soma += valores[i];
+            }
+
+            media = (double)soma / 10;
+
+            for (int i = 0; i < 10; i++)
+            {
+                if (valores[i] > media)
+                {
+                    contador++;
+                }
+            }
+
+            Console.WriteLine("\nMédia: {0}", media);
+            Console.WriteLine("\nQuantidade de valores maiores que a média: {0}\n", contador);
+        }
+    }
+}
